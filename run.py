ab8f172c49eea918a0ebf3a23a2204d8ca2721de
rom <rom.sharon98@gmail.com>
2023-02-27 23:15:15 +0200

    add aadam class as optimizer

--- run.py
+++ run.py
@@ -1,11 +1,66 @@
 # example of plotting the adam search on a contour plot of the test function
 from math import sqrt
-from numpy import asarray, absolute
+
+import numpy as np
+from matplotlib import pyplot
 from numpy import arange, sign
+from numpy import asarray, absolute
+from numpy import meshgrid
 from numpy.random import rand
 from numpy.random import seed
-from numpy import meshgrid
-from matplotlib import pyplot
+from sklearn.neural_network._stochastic_optimizers import AdamOptimizer
+
+
+class AAdamOptimizer(AdamOptimizer):
+    def __init__(self, params):
+        super().__init__(params)
+        self.previous_update = [np.zeros_like(param) for param in params]
+        self.previous_previous_update = [np.zeros_like(param) for param in params]
+        self.ds = [np.zeros_like(param) for param in params]
+
+    def update_params(self, grads):
+        """Update parameters with given gradients
+
+        Parameters
+        ----------
+        grads : list, length = len(params)
+            Containing gradients with respect to coefs_ and intercepts_ in MLP
+            model. So length should be aligned with params
+        """
+        updates = self._get_updates(grads)
+        for param, update in zip(self.params, updates):
+            param += update
+
+    def _get_updates(self, grads):
+        """Get the values used to update params with given gradients
+
+        Parameters
+        ----------
+        grads : list, length = len(coefs_) + len(intercepts_)
+            Containing gradients with respect to coefs_ and intercepts_ in MLP
+            model. So length should be aligned with params
+
+        Returns
+        -------
+        updates : list, length = len(grads)
+            The values to add to params
+        """
+        self.t += 1
+        self.ms = [self.beta_1 * m + (1 - self.beta_1) * grad
+                   for m, grad in zip(self.ms, grads)]
+        self.vs = [self.beta_2 * v + (1 - self.beta_2) * (grad ** 2)
+                   for v, grad in zip(self.vs, grads)]
+        self.learning_rate = (self.learning_rate_init *
+                              np.sqrt(1 - self.beta_2 ** self.t) /
+                              (1 - self.beta_1 ** self.t))
+        self.ds = [
+            absolute(prev - prev_prev) * sign(grad) * (1 - beta1)
+            for prev, prev_prev, grad in zip(self.previous_update, self.previous_previous_update, grads)]
+        updates = [-self.learning_rate * m * beta1 / (np.sqrt(v + self.epsilon)) + d
+                   for m, v, d in zip(self.ms, self.vs, self.ds)]
+        self.previous_previous_update = self.previous_update
+        self.previous_update = updates
+        return updates
 
 
 # objective function
@@ -19,7 +74,7 @@ def derivative(x, y):
 
 
 # gradient descent algorithm with adam
-def adam(objective, derivative, bounds, n_iter, alpha, beta1, beta2, eps=1e-8):
+def aadam(objective, derivative, bounds, n_iter, alpha, beta1, beta2, eps=1e-8):
     solutions = list()
     # generate an initial point
     x = bounds[:, 0] + rand(len(bounds)) * (bounds[:, 1] - bounds[:, 0])
@@ -44,11 +99,11 @@ def adam(objective, derivative, bounds, n_iter, alpha, beta1, beta2, eps=1e-8):
             mhat = m[i] / (1.0 - beta1 ** (t + 1))
             # vhat(t) = v(t) / (1 - beta2(t))
             vhat = v[i] / (1.0 - beta2 ** (t + 1))
-            # x(t) = x(t-1) - alpha * mhat(t) / (sqrt(vhat(t)) + ep)
-            d = absolute((x_before_before[i] - x_before[i])) * sign(g[i]) * (1 - beta1)
+            d = absolute(x_before[i] - x_before_before[i]) * sign(g[i]) * (1 - beta1)
             print(d)
+            # x(t) = x(t-1) - alpha * mhat(t) / (sqrt(vhat(t)) + ep)
             # x[i] = x[i] - alpha * mhat / (sqrt(vhat) + eps)
-            x[i] = x[i] - (alpha *mhat * beta1 / (sqrt(vhat + eps)) + d)
+            x[i] = x[i] - (alpha * beta1 * mhat / (sqrt(vhat + eps)) + d)
         # evaluate candidate point
         score = objective(x[0], x[1])
         x_before_before = x_before
@@ -65,15 +120,15 @@ seed(1)
 # define range for input
 bounds = asarray([[-1.0, 1.0], [-1.0, 1.0]])
 # define the total iterations
-n_iter = 60
+n_iter = 10
 # steps size
 alpha = 0.02
 # factor for average gradient
-beta1 = 0.8
+beta1 = 0.9
 # factor for average squared gradient
 beta2 = 0.999
 # perform the gradient descent search with adam
-solutions = adam(objective, derivative, bounds, n_iter, alpha, beta1, beta2)
+solutions = aadam(objective, derivative, bounds, n_iter, alpha, beta1, beta2)
 # sample input range uniformly at 0.1 increments
 xaxis = arange(bounds[0, 0], bounds[0, 1], 0.1)
 yaxis = arange(bounds[1, 0], bounds[1, 1], 0.1)
