c6b2364745c40ec04baeaafe06b29ec1d5e76d32
CohenOri <33662944+CohenOri@users.noreply.github.com>
2023-03-08 17:30:54 +0200

    Added a basic test plot to validate solver efficiency

--- test.py
+++ test.py
@@ -16,28 +16,34 @@ scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train.reshape(-1, 64))
 X_test = scaler.transform(X_test.reshape(-1, 64))
 
-# Define the model architecture
-model = MLPClassifier(hidden_layer_sizes=(512, 256, 128), activation='relu', solver='adam', alpha=0.001, max_iter=20, batch_size=128, learning_rate_init=0.001)
+# Define the model architecture, warm_start are to use model.fit sequentially, max_iter=1 is in order to run only one epoch for each model.fit
+model = MLPClassifier(hidden_layer_sizes=(512, 256, 128), activation='relu', solver='adam', alpha=0.001, max_iter=1, batch_size=10, learning_rate_init=0.001, shuffle=True, verbose=True, warm_start=True, random_state=1, n_iter_no_change=1000)
 
-# Add dropout regularization to the hidden layers
-model.set_params(**{'hidden_layer_sizes': (512, 256, 128), 'alpha': 0.001, 'solver': 'adam', 'batch_size': 128, 'learning_rate_init': 0.001, 'max_iter': 20, 'shuffle': True, 'tol': 1e-4, 'random_state': 1, 'verbose': True, 'warm_start': False, 'momentum': 0.9, 'nesterovs_momentum': True, 'early_stopping': False, 'validation_fraction': 0.1, 'beta_1': 0.9, 'beta_2': 0.999, 'epsilon': 1e-8, 'n_iter_no_change': 10, 'max_fun': 15000})
 
 # Train the model and save the best model with the highest test set accuracy
-best_model = None
+# best_model = None
 best_test_acc = 0
+best_epoch_num = 1
 test_accs = []
-for i in range(model.max_iter):
-    model.fit(X_train, y_train)
+
+"""
+Limit train test size:
+with no limit one epoch will be enough for over 95% accuracy, and we couldn't compare our solver behaviour efficiently
+"""
+TRAIN_SET_SIZE = 100
+for i in range(30):
+    model.fit(X_train[:TRAIN_SET_SIZE], y_train[:TRAIN_SET_SIZE], )
     test_acc = model.score(X_test, y_test)
     test_accs.append(test_acc)
     if test_acc > best_test_acc:
         best_test_acc = test_acc
-        best_model = MLPClassifier(hidden_layer_sizes=(512, 256, 128), activation='relu', solver='adam', alpha=0.001, max_iter=1, batch_size=128, learning_rate_init=0.001)
-        best_model.coefs_ = model.coefs_
-        best_model.intercepts_ = model.intercepts_
+        best_epoch_num = i
+        # best_model = MLPClassifier(hidden_layer_sizes=(512, 256, 128), activation='relu', solver='adam', alpha=0.001, max_iter=1, batch_size=128, learning_rate_init=0.001)
+        # best_model.coefs_ = model.coefs_
+        # best_model.intercepts_ = model.intercepts_
     print('Epoch', i+1, 'Test accuracy:', test_acc)
 
-print('Best test set accuracy:', best_test_acc)
+print(f'Best test set accuracy: {best_test_acc} found at epoch #{best_epoch_num}')
 
 # Plot the test set accuracy after each epoch
 plt.plot(test_accs)
